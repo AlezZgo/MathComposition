9f05cb3ea41246b5c04b8519ed08789e37ab3aa2
Alexander <alez.grante@gmail.com>
2022-01-15 16:45:53 +0300

    data binding with onClickListeners

--- app/src/main/java/com/example/mathcomposition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/mathcomposition/presentation/GameFinishedFragment.kt
@@ -34,47 +34,7 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListeners()
-        bindViews()
-
-
-    }
-
-    private fun bindViews() {
-        with(binding) {
-            ivResultSmile.setImageResource(getSmileId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.necessary_right_answers_count),
-                args.gameResult.gameSettings.minCountOfRightAnswers
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.your_score),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.necessary_required_percent),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.your_percent),
-                getPercentIfRightAnswers()
-            )
-        }
-    }
-
-    private fun getSmileId(): Int {
-        return if (args.gameResult.isWinner) {
-            R.drawable.ic_happy_smile
-        }else{
-            R.drawable.ic_sad_smile
-        }
-    }
-
-    private fun getPercentIfRightAnswers() = with(args.gameResult){
-        if(countOfQuestions==0){
-            0
-        }else{
-            ((countOfRightAnswers / countOfQuestions.toDouble())*100).toInt()
-        }
+        binding.gameResult = args.gameResult
     }
 
     private fun setupClickListeners() {
--- app/src/main/java/com/example/mathcomposition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/mathcomposition/presentation/GameFragment.kt
@@ -1,20 +1,15 @@
 package com.example.mathcomposition.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.mathcomposition.R
 import com.example.mathcomposition.databinding.FragmentGameBinding
 import com.example.mathcomposition.domain.entity.GameResult
-import com.example.mathcomposition.domain.entity.Level
 
 class GameFragment : Fragment() {
 
@@ -22,23 +17,12 @@ class GameFragment : Fragment() {
 
     private val viewModelFactory by lazy {
         GameViewModelFactory(
-            args.level,requireActivity().application
+            args.level, requireActivity().application
         )
     }
 
     private val viewModel by lazy {
         ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
-    }
-
-    private val tvOptions by lazy {
-        mutableListOf<TextView>().apply {
-            add(binding.tvOptions1)
-            add(binding.tvOptions2)
-            add(binding.tvOptions3)
-            add(binding.tvOptions4)
-            add(binding.tvOptions5)
-            add(binding.tvOptions6)
-        }
     }
 
     private var _binding: FragmentGameBinding? = null
@@ -55,72 +39,23 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         observeViewModel()
-        setClickListenersToOptions()
     }
 
     private fun observeViewModel() {
-        viewModel.question.observe(viewLifecycleOwner) {
-            binding.tvSum.text = it.sum.toString()
-            binding.tvLeftNumber.text = it.visiblenumber.toString()
-            for (i in 0 until tvOptions.size) {
-                tvOptions[i].text = it.options[i].toString()
-            }
-        }
-
-        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
-            binding.pbAnswersProgress.setProgress(it, true)
-        }
-
-        viewModel.enoughCount.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.tvAnwersProgress.setTextColor(color)
-        }
-
-        viewModel.enoughPercent.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.pbAnswersProgress.progressTintList = ColorStateList.valueOf(color)
-        }
-
-        viewModel.minPercent.observe(viewLifecycleOwner) {
-            binding.pbAnswersProgress.secondaryProgress = it
-        }
-
-        viewModel.formattedTime.observe(viewLifecycleOwner) {
-            binding.tvTimer.text = it
-        }
-
         viewModel.gameResult.observe(viewLifecycleOwner) {
             launchGameFinishedFragment(it)
         }
-
-        viewModel.progressAnswers.observe(viewLifecycleOwner){
-            binding.tvAnwersProgress.text = it
-        }
-
-
     }
-
-    private fun setClickListenersToOptions(){
-        for (tvOption in tvOptions){
-            tvOption.setOnClickListener {
-                viewModel.chooseAnswer(tvOption.text.toString().toInt())
-            }
-        }
-    }
-
-    private fun getColorByState(goodState: Boolean): Int {
-        val colorResId = if (goodState) {
-            android.R.color.holo_green_light
-        } else {
-            android.R.color.holo_red_light
-        }
-        return ContextCompat.getColor(requireContext(), colorResId)
-    }
-
 
     private fun launchGameFinishedFragment(gameResult: GameResult) {
-        findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameFinishedFragment(gameResult))
+        findNavController().navigate(
+            GameFragmentDirections.actionGameFragmentToGameFinishedFragment(
+                gameResult
+            )
+        )
     }
 
     override fun onDestroy() {
